016e4c648595d7b006049210042ab4d4b43f7664
allansrc <allanramos.teixeira@gmail.com>
2022-06-10 12:54:04 -0300

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Allan Ramos on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
